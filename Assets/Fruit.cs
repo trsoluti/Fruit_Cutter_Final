78e677786b1c8fc659ab3f095eb17e0d2ace882d
Thomas O'Dell <thomas_odell@trsolutions.biz>
2020-08-29 17:18:47 +0800

    Knife working
    Knife moves with mouse; apples rotate; apples disappear properly.

---
+++ Assets/Fruit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    // Randomly generate the rotate degree of the fruit
+    int rotateDegree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotateDegree = Random.Range(-6, 6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotateDegree);
+
+        // Delete the fruit when it is falling down
+        // y position is less than -10
+        if (transform.position.y < -10)
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+}
---
+++ Assets/Knife.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the current mouse position
+        Vector3 mousepos = new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            1
+            );
+        Vector3 knifepos = Camera.main.ScreenToWorldPoint(mousepos);
+
+        transform.position = knifepos;
+    }
+}
