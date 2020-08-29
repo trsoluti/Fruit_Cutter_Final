59ac52cbc4fb546491e6ec4d352c75859aa6096f
Thomas O'Dell <thomas_odell@trsolutions.biz>
2020-08-29 16:39:12 +0800

    Can throw fruit
    Fruit spawner moves to a random position and spawns the fruit.

---
+++ Assets/FruitThrower.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitThrower : MonoBehaviour
+{
+    public GameObject fruit;
+
+    // Create a variable Timer - Data type Integer
+    int timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Increase the timer every frame
+        timer = timer + 1;
+
+        if (timer > 100)
+        {
+            transform.position = new Vector3(
+                Random.Range(-10, 10),
+                -8,
+                0
+                );
+
+            // Clone the new fruit from existing fruit prefab
+            // at the fruit thrower position
+            GameObject newFruit = Instantiate(
+                fruit,
+                transform.position,
+                transform.rotation
+                );
+            newFruit.GetComponent<Rigidbody2D>().AddForce(
+                Vector3.up * 800
+                );
+
+            // Add the side force (left or right) to the new fruit
+            // for swing when throwing upwards
+            newFruit.GetComponent<Rigidbody2D>().AddForce(
+                Vector3.left * Random.Range(-400, 400)
+                );
+            timer = 0;
+        }
+    }
+}
